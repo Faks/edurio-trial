520fcf4573bbade75a6c2ed16512b943b494db76
Oskars Germovs <sia_dev@inbox.lv>
2020-10-05 10:30:01 +0300

    updates

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-06-18 11:52:15.
+    * Generated on 2019-06-18 15:32:23.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
