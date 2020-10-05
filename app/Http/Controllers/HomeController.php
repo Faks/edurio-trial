65920a379e85fcc288d478e45d5aa996310f6243
Oskars Germovs <sia_dev@inbox.lv>
2020-10-05 10:41:35 +0300

    Added Links

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Source;
-use function collect;
-use function dump;
-use const PHP_EOL;
 use SplFixedArray;
+
+use function collect;
+
+use const PHP_EOL;
 
 class HomeController
 {
@@ -32,11 +33,23 @@ class HomeController
      * @var array
      */
     private static $getCsvHeader = [
-        'a', 'b', 'c',
+        'a',
+        'b',
+        'c',
     ];
 
-    public function show() : void
+    public function show(): void
     {
+        echo '<ul>
+<li class="nav-item">
+                        <a class="nav-link" href="https://bitbucket.org/Faks/edurio-trial/src/master/">Bitbucket</a>
+                    </li>
+<li class="nav-item">
+                        <a class="nav-link" href="https://www.linkedin.com/in/oskars-germovs-a94b3318a/">LinkedIn</a>
+                    </li>
+                    </ul>
+                    <p></p>';
+
         $getSource = Source::select(['a', 'b', 'c'])->get();
 
         $file = \fopen('php://output', 'w');
@@ -47,11 +60,11 @@ class HomeController
         }
     }
 
-    public function store() : void
+    public function store(): void
     {
         foreach ($this->getMillionArrayChunk($this->setRange) as $getMillionArrayChunkDepthFirst) {
             foreach ($getMillionArrayChunkDepthFirst as $getMillionArrayChunkDepthSecond) {
-                $storeSource    = new Source();
+                $storeSource = new Source();
                 $storeSource->a = $getMillionArrayChunkDepthSecond;
                 $storeSource->b = $getMillionArrayChunkDepthSecond / 100 * 3;
                 $storeSource->c = $getMillionArrayChunkDepthSecond / 100 * 5;
@@ -65,7 +78,7 @@ class HomeController
      *
      * @return SplFixedArray
      */
-    private function getMillionArray($getOneMillion) : SplFixedArray
+    private function getMillionArray($getOneMillion): SplFixedArray
     {
         $getMillionArray = new SplFixedArray($getOneMillion);
 
@@ -83,7 +96,7 @@ class HomeController
      *
      * @return array
      */
-    private function getMillionArrayChunk($getOneMillion) : array
+    private function getMillionArrayChunk($getOneMillion): array
     {
         return collect($this->getMillionArray($getOneMillion))
             ->chunk($this->setChunkStep)
